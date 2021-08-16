4b8f42996207150adc719fca555bc19bfef8dae0
danoxv <65130664+danoxv@users.noreply.github.com>
2021-08-16 12:29:35 +0300

    add new code

--- 55.php
+++ 55.php
@@ -1,1 +1,27 @@
 <?php
+
+class User
+{
+    private $name;
+    private $age;
+
+    public function __set($prop, $val)
+    {
+        if ($prop != '' || $val >= 0 && $val <= 70) { // проверяем имя на непустоту
+            $this->$prop = $val;
+        }
+
+    }
+
+    public function __get($prop)
+    {
+        return $this->$prop;
+    }
+}
+
+$u = new User;
+$u->name = "Игорь";
+$u->age = 25;
+echo $u->name;
+echo $u->age;
+?>
